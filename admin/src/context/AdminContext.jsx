56a3a1c48d1c4910d46a2bc010d663281790d648
RameesRaja-M <rameesraja27082002@gmail.com>
2025-09-10 14:20:52 +0530

    Prescripto Completed

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { createContext } from "react";
 import axios from 'axios';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 
 export const AdminContext = createContext();
 
@@ -9,18 +9,20 @@ const AdminContextProvider = (props) => {
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : "");
     const [doctors, setDoctors] = useState([]);
+    const [appointments, setAppointments] = useState([]);
+    const [dashData, setDashData] = useState(false);
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
 
         try {
-            
-            const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, {headers: {aToken}});
+
+            const { data } = await axios.post(backendUrl + '/api/admin/all-doctors', {}, { headers: { aToken } });
             if (data.success) {
                 setDoctors(data.doctors);
                 console.log(data.doctors);
-                
+
             } else {
                 toast.error(data.message);
             }
@@ -32,12 +34,66 @@ const AdminContextProvider = (props) => {
     const changeAvailablity = async (docId) => {
 
         try {
-            
-            const {data} = await axios.post(backendUrl + '/api/admin/change-availablity', {docId},{headers: {aToken}});
-            
+
+            const { data } = await axios.post(backendUrl + '/api/admin/change-availablity', { docId }, { headers: { aToken } });
+
             if (data.success) {
                 toast.success(data.message);
                 getAllDoctors();
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    const getAllAppointments = async (req, res) => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/appointments', { headers: { aToken } });
+            if (data.success) {
+                setAppointments(data.appointments);
+                console.log(data.appointments);
+
+            } else {
+                toast.error(data.error)
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+
+        try {
+
+            const { data } = await axios.post(backendUrl + '/api/admin/cancel-appointment', { appointmentId }, { headers: { aToken } });
+            if (data.success) {
+                toast.success(data.message);
+                getAllAppointments()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    const getDashData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } });
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
             } else {
                 toast.error(data.message);
             }
@@ -52,7 +108,11 @@ const AdminContextProvider = (props) => {
         backendUrl,
         doctors,
         getAllDoctors,
-        changeAvailablity
+        changeAvailablity,
+        appointments, setAppointments,
+        getAllAppointments,
+        cancelAppointment,
+        dashData,getDashData
     }
 
     return (
--- admin/src/pages/Doctor/DoctorDashboard.jsx
+++ admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import { useContext } from 'react'
-import { AdminContext } from '../../context/AdminContext'
+import { DoctorContext } from '../../context/DoctorContext'
 import { useEffect } from 'react';
 import { assets } from '../../assets/assets';
 import { AppContext } from '../../context/AppContext';
 
-const Dashboard = () => {
+const DoctorDashboard = () => {
 
-  const { aToken, getDashData, cancelAppointment, dashData } = useContext(AdminContext);
-  const { slotDateFormat } = useContext(AppContext);
+  const { dToken, dashData, setDashData, getDashData, completeAppointment, cancelAppointment } = useContext(DoctorContext);
+  const { currency, slotDateFormat } = useContext(AppContext);
+
 
   useEffect(() => {
-    if (aToken) {
-      getDashData();
+    if (dToken) {
+      getDashData()
     }
-  }, [aToken])
+  }, [dToken])
 
   return dashData && (
     <div className='m-5'>
       <div className='flex flex-wrap gap-3'>
 
         <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
-          <img className='w-14' src={assets.doctor_icon} alt="Doctor Icon" />
+          <img className='w-14' src={assets.earning_icon} alt="Doctor Icon" />
           <div>
-            <p className='text-xl font-semibold text-gray-600'>{dashData.doctors}</p>
-            <p>Doctors</p>
+            <p className='text-xl font-semibold text-gray-600'>{currency} {dashData.earnings}</p>
+            <p>Earnings</p>
           </div>
         </div>
 
@@ -57,18 +58,22 @@ const Dashboard = () => {
           {
             dashData.latestAppointments.map((item, index) => (
               <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
-                <img className='w-10 rounded-full' src={item.docData.image} alt="Doctor Image" />
+                <img className='w-10 rounded-full' src={item.userData.image} alt="Doctor Image" />
                 <div className='flex-1 text-sm'>
-                  <p className='text-gray-800 font-medium'>{item.docData.name}</p>
+                  <p className='text-gray-800 font-medium'>{item.userData.name}</p>
                   <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
                 </div>
                 {
-                  item.cancelled
-                    ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
-                    : item.isCompleted ?
+                  item.cancelled ?
+                    <p className='text-red-500 text-xs font-medium'>Cancelled</p>
+                    :
+                    item.isCompleted ?
                       <p className='text-green-500 text-xs font-medium'>Completed</p>
                       :
-                      <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="Cancel Icon" />
+                      <div className='flex'>
+                        <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="Cancel Icon" />
+                        <img onClick={() => completeAppointment(item._id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="CheckBox Icon" />
+                      </div>
                 }
               </div>
             ))
@@ -82,4 +87,4 @@ const Dashboard = () => {
   )
 }
 
-export default Dashboard
+export default DoctorDashboard
